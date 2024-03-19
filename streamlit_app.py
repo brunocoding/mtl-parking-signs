5eb28b1264725d5082e81208a73d6d513fde7920
bcarvalho <bcarvalho@viascience.com>
2024-03-19 18:22:58 -0400

    minor refactoring

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,35 +6,23 @@ import plotly.express as px
 
 us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
 
-with open('https://donnees.montreal.ca/dataset/8ac6dd33-b0d3-4eab-a334-5a6283eb7940/resource/52cecff0-2644-4258-a2d1-0c4b3b116117/download/signalisation_stationnement.geojson') as f:
-    gj = geojson.load(f)
+from urllib import request
+
+with request.urlopen('https://donnees.montreal.ca/dataset/8ac6dd33-b0d3-4eab-a334-5a6283eb7940/resource/52cecff0-2644-4258-a2d1-0c4b3b116117/download/signalisation_stationnement.geojson') as f:
+    # file = print(f.read().decode())
+    gj = geojson.loads(f.read())
 
 fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
                         color_discrete_sequence=["fuchsia"], zoom=3, height=800)
 fig.update_layout(
-    mapbox_style="white-bg",
+    mapbox_style="open-street-map",
     mapbox_layers=[
-        {
-            "below": 'traces',
-            "sourcetype": "raster",
-            "sourceattribution": "United States Geological Survey",
-            "source": [
-                "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}"
-            ]
-        },
-        {
-            "sourcetype": "raster",
-            "sourceattribution": "Government of Canada",
-            "source": ["https://geo.weather.gc.ca/geomet/?"
-                       "SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX={bbox-epsg-3857}&CRS=EPSG:3857"
-                       "&WIDTH=1000&HEIGHT=1000&LAYERS=RADAR_1KM_RDBR&TILED=true&FORMAT=image/png"],
-        },
         {
             "name": "Example",
             "below": 'traces',
             "opacity": 0.6,
             "sourcetype": "geojson",
-            "type": "fill",
+            "type": "circle",
             "color": "royalblue",
             "source": gj
         }
