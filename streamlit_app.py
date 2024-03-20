33f621a2934e01c279e7a6279c9c8df163c62325
bcarvalho <bcarvalho@viascience.com>
2024-03-20 11:43:39 -0400

    use start zoom level=8

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,12 +37,12 @@ st.markdown(
 )
 mtl_parking_signs = pd.read_csv("data/signalisation_stationnement.csv")
 fig = px.scatter_mapbox(mtl_parking_signs, lat="Latitude", lon="Longitude", hover_name="Quartier", hover_data={'Latitude':False, 'Longitude':False, 'Description':True, 'Depuis/Since': True},
-                        color_discrete_sequence=["red"], zoom=10, height=700)
+                        color_discrete_sequence=["red"], zoom=8, height=700)
 fig.update_layout(
     mapbox_style="open-street-map"
 )
 fig.update_mapboxes(center={"lat":45.5517,"lon":-73.7073})
-fig.update_mapboxes(zoom=10)
+fig.update_mapboxes(zoom=8)
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 # Plot!
 st.plotly_chart(fig, use_container_width=True)
