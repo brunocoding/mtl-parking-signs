aeca62e186bc0d120906e6e6ed3628cf34005414
bcarvalho <bcarvalho@viascience.com>
2024-03-20 09:46:46 -0400

    change zoom settings

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,12 @@ import plotly.express as px
 mtl_parking_signs = pd.read_csv("data/signalisation_stationnement.csv")
 
 fig = px.scatter_mapbox(mtl_parking_signs, lat="Latitude", lon="Longitude", hover_name="Quartier", hover_data={'Latitude':False, 'Longitude':False, 'Description':True, 'Depuis/Since': True},
-                        color_discrete_sequence=["red"], zoom=3, height=800)
+                        color_discrete_sequence=["red"], zoom=9, height=800)
 fig.update_layout(
     mapbox_style="open-street-map"
 )
 fig.update_mapboxes(center={"lat":45.5517,"lon":-73.7073})
-fig.update_mapboxes(zoom=8)
+fig.update_mapboxes(zoom=9)
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
 # Plot!
